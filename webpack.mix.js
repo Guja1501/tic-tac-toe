d228382c1c7a5aa846b7e18f294fc6d712ec70dd
Guja Babunashvili <Guja1501@gmail.com>
2018-10-29 14:16:52 +0400

    fix: github pages

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/assets/js')
-	.sass('resources/assets/sass/app.scss', 'public/assets/css');
+mix.js('resources/assets/js/app.js', 'docs/assets/js')
+	.sass('resources/assets/sass/app.scss', 'docs/assets/css');
 
